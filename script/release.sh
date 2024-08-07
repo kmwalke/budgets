b81868fe4e5b57ee1293671f8e925bb99813c53e
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-07 06:58:59 -0600

    add logging to release script

--- script/release.sh
+++ script/release.sh
@@ -9,8 +9,9 @@ psql $DATABASE_URL -c 'SELECT name FROM muni_types LIMIT 1'
 
 if [ $? == 0 ]; then
   echo "Running Rails DB migrations..."
-  bundle exec rails db:migrate
-  bundle exec rails db:seed
+  bundle exec rails db:migrate && echo "migrations complete"
+  echo "Running Rails DB seeds..."
+  bundle exec rails db:seed && echo "seeds complete"
 else
   echo 'Skipping Rails DB migrations!!'
   echo 'If this is happening outside review app provisioning something went wrong!'
