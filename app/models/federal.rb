4e4de6ff20b9b3f2221ec797444804e49643a363
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-10 10:06:22 -0600

    add proper inverse_ofs

--- app/models/federal.rb
+++ app/models/federal.rb
@@ -1,3 +1,3 @@
 class Federal < Municipality
-  has_many :states, class_name: 'State', foreign_key: 'municipality_id'
+  has_many :states, class_name: 'State', foreign_key: 'municipality_id', inverse_of: :federal
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,6 @@
 class State < Municipality
   belongs_to :federal, class_name: 'Federal', foreign_key: :municipality_id, inverse_of: :states
-  has_many :counties, class_name: 'County', foreign_key: 'municipality_id'
+  has_many :counties, class_name: 'County', foreign_key: 'municipality_id', inverse_of: :state
 
   def cities
     City.joins(:county).where(county: { state: self })
