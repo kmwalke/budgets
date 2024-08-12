56cb2f0713efd98a7602208a7af5b670137826f4
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-12 08:58:31 -0600

    build out state spec

--- spec/features/state_spec.rb
+++ spec/features/state_spec.rb
@@ -3,29 +3,23 @@ require 'rails_helper'
 RSpec.describe 'State' do
   let!(:state) { create(:state) }
 
-  before :all do
-    create(:city, county: create(:county, state:))
-  end
-
-  before :each do
+  xit 'shows the state budget' do
     visit state_path(state)
-  end
-
-  xit 'shows the federal budget' do
     expect(true).to be_nil
   end
 
-  it 'lists the states' do
-    expect(page).to have_content(state.name)
+  it 'lists the counties' do
+    visit state_path(state)
+    expect(page).to have_content(county.name)
   end
 
   it 'links to a county page' do
-    click_link county.name
+    visit state_path(state)
     expect(page).to have_current_path(county), ignore_query: true
   end
 
   it 'links to a city page' do
-    click_link city.name
+    visit state_path(state)
     expect(page).to have_current_path(city), ignore_query: true
   end
 end
