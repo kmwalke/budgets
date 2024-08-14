c0d80f5bea2a6973b686808365c640844c919031
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-14 13:33:10 -0600

    add department chart

--- spec/features/department_spec.rb
+++ spec/features/department_spec.rb
@@ -2,39 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Department' do
   before do
-    @state = create(:state)
-    budget = create(:budget, department: create(:department, municipality: @state))
+    @department = create(:department)
+    budget      = create(:budget, department: @department)
     create_list(:line_item, 10, budget:)
-    visit state_path(@state)
+    visit department_path(@department)
   end
 
-  it 'shows the latest state budget' do
-    expect(page).to have_content(@state.departments.first.latest_budget.year)
+  it 'shows the latest department budget' do
+    expect(page).to have_content(@department.latest_budget.year)
   end
 
-  it 'shows the departments' do
-    @state.departments.each do |department|
-      expect(page).to have_content(department.name)
+  it 'shows the line_items' do
+    @department.latest_budget.line_items.each do |line_item|
+      expect(page).to have_content(line_item.name)
     end
-  end
-
-  it 'lists the counties' do
-    county = create(:county, state: @state)
-    visit state_path(@state)
-    expect(page).to have_content(county.name)
-  end
-
-  it 'links to a county page' do
-    county = create(:county, state: @state)
-    visit state_path(@state)
-    click_on county.name
-    expect(page).to have_current_path(county_path(county))
-  end
-
-  it 'links to a city page' do
-    city = create(:city, county: create(:county, state: @state))
-    visit state_path(@state)
-    click_on city.name
-    expect(page).to have_current_path(city_path(city))
   end
 end
