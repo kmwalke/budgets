df7e6686b49eb585def8d1cfe3484eead5ec13fd
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-14 13:23:56 -0600

    change homecontroller to fedcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root 'home#index'
+  root 'federals#show'
 
+  get '/federals', to: 'federals#show', as: 'federal'
   get '/states/:id', to: 'states#show', as: 'state'
   get '/counties/:id', to: 'counties#show', as: 'county'
   get '/cities/:id', to: 'cities#show', as: 'city'
