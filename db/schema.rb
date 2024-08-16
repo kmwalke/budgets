1f6b638a749123456e0974a57156739ef9c1e318
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-16 13:15:40 -0600

    formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_240_816_123_109) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_16_123109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
