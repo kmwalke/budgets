869fa478869bd1f65aee807b755cb73061a57ea3
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-18 10:49:52 -0600

    additional budget specs

--- spec/features/budgets_spec.rb
+++ spec/features/budgets_spec.rb
@@ -3,23 +3,19 @@ require 'rails_helper'
 RSpec.describe 'Budget' do
   before do
     @state  = create(:state)
-    @budget = create(:budget, department: create(:department, municipality: @state))
-    create_list(:line_item, 10, budget: @budget)
     visit state_path(@state)
   end
 
   describe 'logged out' do
     it 'does not show draft budget' do
-      budget = create(:budget, department: create(:department, municipality: @state), status: MuniStatus::DRAFT)
-      create_list(:line_item, 10, budget:)
+      budget = create_budget
       visit state_path(@state)
 
       expect(page).to have_no_content(budget.year)
     end
 
     it 'shows live budget' do
-      budget = create(:budget, department: create(:department, municipality: @state), status: MuniStatus::LIVE)
-      create_list(:line_item, 10, budget:)
+      budget = create_budget(status: MuniStatus::LIVE)
       visit state_path(@state)
 
       expect(page).to have_content(budget.year)
@@ -40,18 +36,33 @@ RSpec.describe 'Budget' do
       expect(page).to have_current_path(state_path(@state))
     end
 
+    it 'activates a budget' do
+      budget = create_budget(status: MuniStatus::DRAFT)
+      visit state_path(@state)
+
+      click_on 'Activate Budget'
+
+      expect(budget.reload.status).to eq(MuniStatus::LIVE)
+    end
+
     it 'shows draft budgets' do
-      budget = create(:budget, department: create(:department, municipality: @state), status: MuniStatus::DRAFT)
-      create_list(:line_item, 10, budget:)
+      budget = create_budget
       visit state_path(@state)
 
       expect(page).to have_content(budget.year)
     end
 
     it 'triggers an import on attaching a csv', skip: 'not implemented' do
-      budget = create(:budget, department: create(:department, municipality: @state), status: MuniStatus::DRAFT)
+      budget = create_budget
       budget.csv.attach('csv')
       expect(true).to be_nil
     end
   end
+end
+
+def create_budget(status: MuniStatus::DRAFT)
+  budget = create(:budget, department: create(:department, municipality: @state), status:)
+  create_list(:line_item, 10, budget:)
+
+  budget
 end
