792e1d721ad8532b7c1ee362f531c2ef1a899a41
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-18 09:02:51 -0600

    initial budget specs

--- spec/features/budgets_spec.rb
+++ spec/features/budgets_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Budget' do
   before do
-    @state  = create(:state)
+    @state = create(:state)
     @budget = create(:budget, department: create(:department, municipality: @state))
     create_list(:line_item, 10, budget: @budget)
     visit state_path(@state)
   end
 
-  describe 'logged out' do
+  describe 'logged out', skip: 'not implemented' do
     it 'does not show draft budget' do
       expect(true).to be_nil
     end
@@ -19,15 +19,24 @@ RSpec.describe 'Budget' do
   end
 
   describe 'logged in' do
+    before do
+      login_as_user
+      visit state_path(@state)
+    end
+
     it 'creates a budget' do
-      expect(true).to be_nil
+      click_on 'New Budget'
+      fill_in 'Year', with: 2024
+      click_on 'Create Budget'
+
+      expect(page).to have_current_path(state_path(@state))
     end
 
     it 'shows draft budgets' do
       expect(true).to be_nil
     end
 
-    it 'triggers an import on attaching a csv' do
+    it 'triggers an import on attaching a csv', skip: 'not implemented' do
       expect(true).to be_nil
     end
   end
