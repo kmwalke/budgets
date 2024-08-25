8106263306922d19710aacdaa1031b9296749aaf
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-25 07:27:15 -0600

    order departments and line_items by amount

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,6 @@
 class CitiesController < ApplicationController
   def show
-    @city = City.find(params[:id])
+    @city                = City.find(params[:id])
+    @ordered_departments = ordered_departments(@city)
   end
 end
