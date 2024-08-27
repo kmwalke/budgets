2850c1a98b2c360978546f93b8f6908f11c49502
Kent Slaymaker <kentslaymaker@gmail.com>
2024-08-26 19:39:01 -0600

    formatting

--- spec/classes/budget_importer_spec.rb
+++ spec/classes/budget_importer_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe BudgetImporter do
   it 'has nil handling' do
-    expect(BudgetImporter.import(nil)).to be_nil
+    expect(described_class.import(nil)).to be_nil
   end
 end
