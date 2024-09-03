4e7016d3b292a9b4eafd540978dd01f66c750abb
Kent Slaymaker <kentslaymaker@gmail.com>
2024-09-03 08:36:42 -0600

    formatting

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_02_123705) do
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index %w[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness",
-                                                    unique: true
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -40,7 +39,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_02_123705) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index %w[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "app_errors", force: :cascade do |t|
